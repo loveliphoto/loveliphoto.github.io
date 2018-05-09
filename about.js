c1ff58c18cc93f8c3dfc429445a05e8bf4989e5a
Ben Li <liebennyboy@gmail.com>
2018-05-09 22:24:30 +0800

    butt buttons

--- about.js
+++ about.js
@@ -3,15 +3,23 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 	document.getElementById('bettyButton').addEventListener('click', bettyButton);
 	document.getElementById('benButton').addEventListener('click', benButton);
+	document.getElementById('bettyButt').addEventListener('click', bettyButton);
+	document.getElementById('benButt').addEventListener('click', benButton);
 });
 
 function bettyButton(event) {
+	document.getElementById('bettyButton').classList.toggle('hidden');
+	document.getElementById('bettyButt').classList.toggle('hidden');
+
 	var classList = document.getElementById('li').classList;
 	classList.toggle('center');
 	classList.toggle('left');
 }
 
 function benButton(event) {
+	document.getElementById('benButton').classList.toggle('hidden');
+	document.getElementById('benButt').classList.toggle('hidden');
+
 	var classList = document.getElementById('li').classList;
 	classList.toggle('center');
 	classList.toggle('right');
