00c77fd9561dc923320aae53dc047c58012c5ee9
Ben Li <liebennyboy@gmail.com>
2018-06-02 12:20:06 +0800

    portfolio complete!
    Next step is to smooth out the transition

--- portfolio.js
+++ portfolio.js
@@ -7,34 +7,20 @@ class Portfolio extends HTMLElement {
 }
 customElements.define('li-portfolio', Portfolio);
 
-let START_INDEX;
-let index;
+let START_INDEX = [];
+let photoIndex = [];
 let tx = [];
+// pIndex - portfolio index
 
 window.addEventListener('load', function(event) {
-	let carousel = document.getElementById('portfolio-seniors').firstElementChild;
-	let photos = carousel.children;
-
-	START_INDEX = Math.floor(photos.length / 2);
-	index = START_INDEX;
-
-	let t = 0;
-
-	for (let i = 0; i < START_INDEX; i++) {
-		t += photos[i].clientWidth;
-	}
-
-	t -= ((carousel.clientWidth - photos[START_INDEX].clientWidth) / 2);
-
-	for (let img of photos) {
-		img.addEventListener('transitionend', visible);
-		img.style.transform = 'translate3d(' + (-1) * t + 'px, 0, 0)';
-		tx.push(t);
+	let carousels = document.querySelectorAll('.carousel');
+	for (let pIndex = 0; pIndex < carousels.length; pIndex++) {
+		initCarousel(carousels[pIndex], pIndex);
 	}
 
 	let xButtons = document.querySelectorAll('.portfolio-x-button');
-	for (let xButton of xButtons) {
-		xButton.addEventListener('click', togglePortfolio);
+	for (let button of xButtons) {
+		button.addEventListener('click', closePortfolio);
 	}
 
 	let leftArrows = document.querySelectorAll('.portfolio-left-arrow-div');
@@ -47,37 +33,73 @@ window.addEventListener('load', function(event) {
 		arrow.firstElementChild.addEventListener('click', scrollRight);
 	}
 
-	document.getElementById('portfolio-seniors-button').addEventListener('click', togglePortfolio);
+	let portfolioLinks = document.querySelectorAll('.portfolio-link');
+	for (let link of portfolioLinks) {
+		link.addEventListener('click', openPortfolio);
+	}
 });
 
+function initCarousel(carousel, pIndex) {
+	let photos = carousel.children;
+
+	START_INDEX.push(Math.floor(photos.length / 2));
+	photoIndex.push(START_INDEX[pIndex]);
+
+	let t = 0;
+
+	for (let i = 0; i < START_INDEX[pIndex]; i++) {
+
 function visible(event) {
-	event.target.style.opacity = 1;
-	event.target.removeEventListener('transitionend', visible);
+	event.currentTarget.style.opacity = 1;
+	event.currentTarget.removeEventListener('transitionend', visible);
 }
 
-function togglePortfolio(event) {
-	document.body.classList.toggle('portfolio-view');
-	let portfolio = document.getElementById('portfolio-seniors');
 
-	if (window.getComputedStyle(portfolio).zIndex == -1) {
-		portfolio.style.zIndex = 1;
-		portfolio.style.opacity = 1;
-	} else {
-		portfolio.style.opacity = 0;
-		portfolio.addEventListener('transitionend', zHide);
+
+function openPortfolio(event) {
+	document.body.classList.toggle('portfolio-view');
+
+	let links = event.currentTarget.parentElement.children;
+	let pIndex = 0;
+	while (pIndex < links.length) {
+		if (links[pIndex] == event.currentTarget) {
+			break;
+		}
+		pIndex++;
 	}
+	
+	let portfolio = document.querySelectorAll('li-portfolio')[pIndex];
+	portfolio.style.zIndex = 1;
+	portfolio.style.opacity = 1;
+}
+
+function closePortfolio(event) {
+	document.body.classList.toggle('portfolio-view');
+
+	let portfolio = event.currentTarget.parentElement;
+	portfolio.style.opacity = 0;
+	portfolio.addEventListener('transitionend', zHide);
 }
 
 function zHide(event) {
-	event.target.style.zIndex = -1;
-	event.target.removeEventListener('transitionend', zHide);
+	event.currentTarget.style.zIndex = -1;
+	event.currentTarget.removeEventListener('transitionend', zHide);
 }
 
 function scrollLeft(event) {
-	let carousel = document.getElementById('portfolio-seniors').firstElementChild;
+	let carousel = event.currentTarget.parentElement.parentElement.firstElementChild;
 	let photos = carousel.children;
 
-	let endIndex = index + photos.length - 1 - START_INDEX;
+	let carousels = document.querySelectorAll('.carousel');
+	let pIndex = 0;
+	while (pIndex < carousels.length) {
+		if (carousels[pIndex] == carousel) {
+			break;
+		}
+		pIndex++;
+	}
+
+	let endIndex = photoIndex[pIndex] + photos.length - 1 - START_INDEX[pIndex];
 	if (endIndex >= photos.length) {
 		endIndex %= photos.length;
 	}
@@ -86,34 +108,43 @@ function scrollLeft(event) {
 	photos[endIndex].addEventListener('transitionend', visible);
 
 	for (let i = 0; i < photos.length; i++) {
-		tx[endIndex] += photos[i].clientWidth;
+		tx[pIndex][endIndex] += photos[i].clientWidth;
 	}
 
-	let leftIndex = index - 1;
+	let leftIndex = photoIndex[pIndex] - 1;
 	if (leftIndex < 0) {
 		leftIndex += photos.length;
 	}
 
 	let t = photos[leftIndex].clientWidth +
 		((carousel.clientWidth - photos[leftIndex].clientWidth) / 2) -
-		((carousel.clientWidth - photos[index].clientWidth) / 2);
+		((carousel.clientWidth - photos[photoIndex[pIndex]].clientWidth) / 2);
 
 	for (let i = 0; i < photos.length; i++) {
-		tx[i] -= t;
-		photos[i].style.transform = 'translate3d(' + (-1) * tx[i] + 'px, 0, 0)';
+		tx[pIndex][i] -= t;
+		photos[i].style.transform = 'translate3d(' + (-1) * tx[pIndex][i] + 'px, 0, 0)';
 	}
 
-	index--;
-	if (index < 0) {
-		index += photos.length;
+	photoIndex[pIndex]--;
+	if (photoIndex[pIndex] < 0) {
+		photoIndex[pIndex] += photos.length;
 	}
 }
 
 function scrollRight(event) {
-	let carousel = document.getElementById('portfolio-seniors').firstElementChild;
+	let carousel = event.currentTarget.parentElement.parentElement.firstElementChild;
 	let photos = carousel.children;
 
-	let startIndex = index - START_INDEX;
+	let carousels = document.querySelectorAll('.carousel');
+	let pIndex = 0;
+	while (pIndex < carousels.length) {
+		if (carousels[pIndex] == carousel) {
+			break;
+		}
+		pIndex++;
+	}
+
+	let startIndex = photoIndex[pIndex] - START_INDEX[pIndex];
 	if (startIndex < 0) {
 		startIndex += photos.length;
 	}
@@ -122,25 +153,25 @@ function scrollRight(event) {
 	photos[startIndex].addEventListener('transitionend', visible);
 
 	for (let i = 0; i < photos.length; i++) {
-		tx[startIndex] -= photos[i].clientWidth;
+		tx[pIndex][startIndex] -= photos[i].clientWidth;
 	}
 
-	let rightIndex = index + 1;
+	let rightIndex = photoIndex[pIndex] + 1;
 	if (rightIndex >= photos.length) {
 		rightIndex %= photos.length;
 	}
 
 	let t = carousel.clientWidth -
-		((carousel.clientWidth - photos[index].clientWidth) / 2) - 
+		((carousel.clientWidth - photos[photoIndex[pIndex]].clientWidth) / 2) - 
 		((carousel.clientWidth - photos[rightIndex].clientWidth) / 2);
 
 	for (let i = 0; i < photos.length; i++) {
-		tx[i] += t;
-		photos[i].style.transform = 'translate3d(' + (-1) * tx[i] + 'px, 0, 0)';
+		tx[pIndex][i] += t;
+		photos[i].style.transform = 'translate3d(' + (-1) * tx[pIndex][i] + 'px, 0, 0)';
 	}
 
-	index++;
-	if (index >= photos.length) {
-		index %= photos.length;
+	photoIndex[pIndex]++;
+	if (photoIndex[pIndex] >= photos.length) {
+		photoIndex[pIndex] %= photos.length;
 	}
 }
